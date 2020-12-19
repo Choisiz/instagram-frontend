3d8e95ad41bac067843f4e831cb18da7fb855b69
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2020-12-20 00:31:04 +0900

    ff

---
+++ src/Components/Router.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Proptype from "prop-types";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Feed from "../Routes/Feed";
+import Auth from "../Routes/Auth";
+
+
+const LoggedInRoutes = () => <><Route exact path="/" component={Feed}></Route></>
+
+const LoggedOutRoutes = () => <><Route exact path="/" component={Auth}></Route></>
+
+const AppRouter = ({isLoggedIn}) => (
+    <Router>
+        <Switch>
+            { isLoggedIn ? <LoggedInRoutes/> : <LoggedOutRoutes/> }
+        </Switch>
+    </Router>
+);
+
+Router.Proptype = {
+    isLoggedIn: Proptype.bool.isRequired
+}
+
+export default AppRouter;
