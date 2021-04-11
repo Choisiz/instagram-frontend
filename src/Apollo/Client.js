e49c4669f818bfed5bd9f022c289289e451da483
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2021-04-11 13:58:00 +0900

    no message

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,10 @@ import ApolloClient from "apollo-boost";
 import {defaults,resolvers} from "./LocalState";
 
 export default new ApolloClient({
-    uri: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://instagram0327-backend.herokuapp.com/",
+    uri: 
+        process.env.NODE_ENV === "production"
+            ? "https://instagram0327-backend.herokuapp.com"
+            : "http://localhost:4000",
     clientState: {
         defaults,
         resolvers
