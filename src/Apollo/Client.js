c66b8ae0f1ebb0b59f79f98ea345a7259814d9c4
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2021-04-12 03:52:34 +0900

    server test3

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import {defaults,resolvers} from "./LocalState";
 
 export default new ApolloClient({
-    uri: process.env.NODE_ENV ==="development" ? "http://localhost:4000" : "https://instagram-prisma-c31956cf67.herokuapp.com/",
+    uri: process.env.NODE_ENV ==="development" ? "https://instagram-prisma-c31956cf67.herokuapp.com":"http://localhost:4000",
     clientState: {
         defaults,
         resolvers
