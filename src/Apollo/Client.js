383f95cf85bd10b07ed992c8a1179ec3b564a16a
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2021-05-12 23:17:12 +0900

    server test

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,14 +1,16 @@
 import ApolloClient from "apollo-boost";
-import {defaults,resolvers} from "./LocalState";
+import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-    uri: "http://localhost:4000",
-    //uri: process.env.NODE_ENV ==="development" ? "https://instagram-prisma-c31956cf67.herokuapp.com":"http://localhost:4000",
-    clientState: {
-        defaults,
-        resolvers
-    },
-    headers: {
-        Authorization : `Bearer ${localStorage.getItem("token")}`
-    }
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "https://instagram-prisma-c31956cf67.herokuapp.com"
+      : "http://localhost:4000",
+  clientState: {
+    defaults,
+    resolvers,
+  },
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
 });
